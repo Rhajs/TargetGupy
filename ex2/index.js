4b126e953ec04d630c986f8dca921583f21e4811
Rhajs <rodrigo.hanjos@gmail.com>
2024-09-04 00:25:32 -0300

    Att

--- ex2/index.js
+++ ex2/index.js
@@ -1,6 +1,7 @@
 let fib = [0, 1];
 let number = 15;
 
+// Laço de repetição para calcular a Fibo
 while (fib.length < 15) {
   let nextValue = fib[fib.length - 1] + fib[fib.length - 2];
   fib.push(nextValue);
@@ -8,4 +9,5 @@ while (fib.length < 15) {
 
 console.log(fib);
 
+// Verificação se a variavel faz parte da Fibo
 console.log(fib.includes(number));
